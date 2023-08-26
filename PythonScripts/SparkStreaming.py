a29ffcf1d2c95886915e8d9075f2804eb9c930dc
Mayank Bhandari <mayank10oct@yahoo.com>
2023-08-27 01:15:42 +0530

    Unit test fixed

--- PythonScripts/SparkStreaming.py
+++ PythonScripts/SparkStreaming.py
@@ -1,7 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-
+from pyspark.sql.functions import col, split, size
 import os
 
 os.environ[
@@ -9,7 +7,7 @@ os.environ[
 ] = "--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 pyspark-shell"
 
 # Create a Spark session
-spark = SparkSession.builder.appName("KafkaJSONProcessing").getOrCreate()
+spark = SparkSession.builder.appName("KafkaWordCount").getOrCreate()
 
 # Define Kafka parameters
 kafka_params = {
@@ -18,30 +16,18 @@ kafka_params = {
     "startingOffsets": "earliest",
 }
 
-# Define the schema for the JSON data
-json_schema = StructType(
-    [
-        StructField("city", StringType(), True),
-        StructField("temperature", IntegerType(), True),
-    ]
-)
-
 # Read data from Kafka topic as a DataFrame
 kafka_df = spark.readStream.format("kafka").options(**kafka_params).load()
 
-# Parse the JSON data and select fields
-parsed_df = kafka_df.selectExpr("CAST(value AS STRING) as json_str")
-parsed_df = parsed_df.select(from_json("json_str", json_schema).alias("data")).select(
-    "data.*"
-)
+# Convert value column to string
+kafka_df = kafka_df.selectExpr("CAST(value AS STRING) as record")
 
-# Perform any processing you need on the parsed_df DataFrame
-# For example, you can calculate statistics or filter data based on conditions
+# Split the record into words and calculate the number of words
+word_count_df = kafka_df.select(size(split(col("record"), " ")).alias("word_count"))
 
-# Display the processed data in the console
-query = parsed_df.writeStream.outputMode("append").format("console").start()
+# Display the word count in the console
+query = word_count_df.writeStream.outputMode("append").format("console").start()
 print(query.status)
-
 
 # Await termination
 query.awaitTermination()
